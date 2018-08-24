6ba64ce36f51566764088292034bea993f50318d
francescolaferla <flaferla@PAV-NB-C02V60VJHTDF.fritz.box>
2018-08-24 15:16:16 +0200

    commit 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React apps</h1>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
-    <Link to="/page-2/">Next</Link>
+    <Link to="/page-2/">Next 2</Link>
 
   </div>
 )
