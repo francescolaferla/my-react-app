3b878f249431af0b485e420709f3eebb7929f499
francescolaferla <flaferla@PAV-NB-C02V60VJHTDF.fritz.box>
2018-08-24 15:14:53 +0200

    Commit 1

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'React test 1',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps</h1>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
+    <Link to="/page-2/">Next</Link>
+
+  </div>
+)
+
+export default IndexPage
